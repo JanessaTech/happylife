d3db6eae11db7ee49329d80f048cd68e02fc6f42
zhaojuan <lvlajzhao1983@gmail.com>
2020-12-10 22:38:55 +0800

    add code generated by mybatis generator

--- src/test/java/com/happylife/core/CoreApplicationTests.java
+++ src/test/java/com/happylife/core/CoreApplicationTests.java
@@ -2,8 +2,10 @@ package com.happylife.core;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
+@TestPropertySource(locations="classpath:application.properties")
 class CoreApplicationTests {
 
 	@Test
